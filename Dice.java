00e1b87d2e061bd58cf3e65448999fe0032aabd4
Brandon <bkreiser5@gmail.com>
2019-02-20 11:18:53 -0500

    09/05/17

---
+++ Dice.java
@@ -0,0 +1,16 @@
+public class Dice
+{
+    public static void main(String[] args)
+    {
+        for (int num1 = 1; num1 <= 6; num1++)
+        {
+            for (int num2 = 1; num2 <=6; num2++)
+            {
+                for (int num3 = 1; num3 <=6; num3++)
+                {
+                    System.out.println(num1 + "" + num2 + "" + num3);
+                }
+            }
+        }
+    }
+}
